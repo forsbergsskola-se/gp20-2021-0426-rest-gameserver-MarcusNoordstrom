606c853af9eac322cf89a02a5d3d9b21b38df6e1
Bulteze <48334398+Bulteze@users.noreply.github.com>
2021-04-28 12:16:29 +0200

    basic finding of the links is available, no functionality yet

--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace TinyBrowser
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static void Main(string[] args) {
+            var tcpClient = new TcpClient();
+            var webSite = "www.acme.com";
+            tcpClient.Connect(webSite, 80);
+            var stream = tcpClient.GetStream();
+            var send = Encoding.ASCII.GetBytes("GET / HTTP/1.1" + Environment.NewLine +
+                                               "Host: acme.com" + Environment.NewLine+Environment.NewLine);
+            stream.Write(send, 0, send.Length);
+            
+            var sr = new StreamReader(stream);
+            var str = sr.ReadToEnd();
+            //Console.WriteLine(str);
+            tcpClient.Close();
+            stream.Close();
+
+            var r = Regex.Matches(str, @"<(a|link).*?href=(""|')(.+?)(""|').*?>").Select(m =>
+                m.Value).ToArray();
+            var re = new RegExp("^(www|http|https)://", "i");
+
+            foreach (var VARIABLE in r) {
+                Console.WriteLine(VARIABLE);
+            }
+            
         }
     }
 }

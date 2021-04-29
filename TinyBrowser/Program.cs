7b2ea7e71b45cefbef9421888fd2671fb7898a1a
Bulteze <48334398+Bulteze@users.noreply.github.com>
2021-04-29 13:37:00 +0200

    TinyBrowser is complete! :100:

--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -1,19 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Text.RegularExpressions;
 
 namespace TinyBrowser
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args) {
+        static void Main() {
             var tcpClient = new TcpClient();
-            var webSite = "www.acme.com";
+            const string webSite = "www.acme.com";
             tcpClient.Connect(webSite, 80);
             var stream = tcpClient.GetStream();
             var send = Encoding.ASCII.GetBytes("GET / HTTP/1.1" + Environment.NewLine +
@@ -26,8 +23,56 @@ namespace TinyBrowser
             tcpClient.Close();
             stream.Close();
 
-            var linkList = GetLinks(str);
+            
+
+            MainLoop(str);
+        }
+
+        static string Navigator(string subDir) {
+            var tcpClient = new TcpClient();
+            var webSite = "www.acme.com";
+            tcpClient.Connect(webSite, 80);
+            var stream = tcpClient.GetStream();
+            var send = Encoding.ASCII.GetBytes($"GET /{subDir} HTTP/1.1" + Environment.NewLine +
+                                               "Host: acme.com" + Environment.NewLine+Environment.NewLine);
+            stream.Write(send, 0, send.Length);
+            
+            var sr = new StreamReader(stream);
+            var str = sr.ReadToEnd();
+            //Console.WriteLine(str);
+            tcpClient.Close();
+            stream.Close();
+            return str;
+        }
+
+        static void MainLoop(string newStr) {
+            var linkList = GetLinks(newStr);
             PrintOutLinks(linkList);
+
+            Dictionary<int, string> inputDict = new Dictionary<int, string>();
+            int index = 0;
+            foreach (var link in linkList) {
+                inputDict.Add(index, link.Key);
+                index++;
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("--Enter a number to go to that link or b/B for going back to the index again--");
+            var input = Console.ReadLine();
+
+            var isNumber = int.TryParse(input, out var num);
+            
+            
+            if (input == "b" || input == "B" || input == "" || !isNumber) {
+                Console.Clear();
+                inputDict.TryGetValue(1000, out var b);
+                MainLoop(Navigator(b));
+            }
+
+            inputDict.TryGetValue(num, out var val);
+            
+            Console.Clear();
+            MainLoop(Navigator(val));
         }
 
         static void PrintOutLinks(Dictionary<string, string> linkList) {
@@ -36,13 +81,10 @@ namespace TinyBrowser
                 Console.WriteLine($"{count} : {link.Key} : {link.Value}");
                 count++;
             }
-            Console.WriteLine();
-            Console.Write("Enter a number to go to that link: ");
         }
 
         static Dictionary<string, string> GetLinks(string str) {
             var result = new Dictionary<string, string>();
-            //Gets links & names
             for (int i = 0; i < str.Length; i++) {
                 var link = "";
                 var name = "";
@@ -58,7 +100,12 @@ namespace TinyBrowser
                             name += str[i];
                             i++;
                         }
-                        result.Add(link, name);
+                        if (result.Count == 0) {
+                            result.Add(link, name);    
+                        }
+                        else if (!result.ContainsKey(link) && !result.ContainsValue(name)) {
+                            result.Add(link, name);
+                        }
                     }
                 }
             }
@@ -68,6 +115,11 @@ namespace TinyBrowser
 }
 
 #region OLD CODE HERE
+
+/* Clearly this approach was bad, i was able to get the links but not,
+ * able to link the correct link to the correct name in a good way.
+ */
+
             //Console.WriteLine(str);
             // var links = Regex.Matches(str, @"<(a|link).*?href=(""|')(.+?)(""|').*?>").Select(m =>
             //     m.Value).ToArray();

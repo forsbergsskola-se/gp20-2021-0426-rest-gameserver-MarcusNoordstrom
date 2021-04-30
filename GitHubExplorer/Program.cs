b0ed7b8573b1adb3441286ce9eef82f2540afc49
Bulteze <48334398+Bulteze@users.noreply.github.com>
2021-04-30 12:06:08 +0200

    Added token & Secrets.json functionality

--- GitHubExplorer/Program.cs
+++ GitHubExplorer/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.IO;
+using System.Text.Json;
 
 namespace GitHubExplorer
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static Secrets LoadAndValidateSecrets() {
+            Secrets secrets;
+            if (!File.Exists("secrets.json")) {
+                secrets = new Secrets();
+                File.WriteAllText("secrets.json", JsonSerializer.Serialize(secrets));
+            }
+            else {
+                secrets = JsonSerializer.Deserialize<Secrets>(File.ReadAllText("secrets.json"));
+            }
+
+            if (string.IsNullOrEmpty(secrets.Token)) {
+                throw new Exception("ERROR: Needs to define a Token in 'secrets.json' file.");
+            }
+
+            return secrets;
+        }
+
+        
+        
+        static void Main(string[] args) {
+            var secrets = LoadAndValidateSecrets();
+            
+            
         }
     }
 }
